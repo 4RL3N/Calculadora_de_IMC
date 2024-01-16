bd48e802903cbc51c5ad5731c80ec3a417b0a3e7
4RL3N <arlen0filho04@gmail.com>
2024-01-15 23:37:21 -0300

    Correcoes de bugs dos botoes, otimizacoes de codigo e adicao do botao limpar

--- index.py
+++ index.py
@@ -1,30 +1,46 @@
 import tkinter as tk
 from tkinter import *
 
-def infos(i):
-    ...
+def infos(x):
+    if x < 18.5:
+        texto = "O valor do seu IMC é considerado baixo, sendo assim, você está abaixo do peso ideal. Atenção!"
+    elif 18.5 <= x < 24.9:
+        texto = "O valor do seu IMC está normal, ou seja, você não está nem acima do peso, nem abaixo."
+    elif 25 <= x <29.9:
+        texto = "O valor do seu IMC é considerado acima do ideal, sendo assim você se enquadra em SOBREPESO. Atenção!"
+    elif 30 <= x < 39.9:
+        texto = "O valor do seu IMC é considerado acima do ideal, sendo assim você se enquadra em OBESIDADE 1. Atenção!"
+    else:
+        texto = "O valor do seu IMC é considerado acima do ideal, sendo assim você se enquadra em OBESIDADE 2. Atenção!"
+    return texto
 
-def resultado(x):
+def janela_popup(x):
     popup = Toplevel()
-    popup.geometry("200x150")
+    popup.geometry("450x100")
     popup.title("Resultado")
-    Label(popup, text=f"Seu IMC é {x:.2f}", font="Helvetica 15 bold").pack()
-    Button(popup, text="Fechar", command=popup.destroy).pack(pady=5)
+    Label(popup, text=f"Seu IMC é de '{x:.2f}'", font="Helvetica 15 bold").pack()
+    texto = infos(x)
+    exibir_texto = Label(popup, text=texto, font="Helvetica 11").pack()
+    botao_fechar = Button(popup, text="Fechar", command=popup.destroy).pack(pady=5)
+
+def limpar():
+    peso.set(options_peso[0])
+    campo_altura.delete(0, "end")
 
 def calcular_imc():
     p2 = int(peso.get())
     altura = float(campo_altura.get())
     imc = p2 / (altura**2)
-    resultado(imc)
+    janela_popup(imc)
 
 janela = tk.Tk()
-janela.geometry("400x250")
+janela.geometry("400x240")
 janela.title("Calculadora de IMC")
 tk.Label(janela, text="Vamos descobrir seu IMC", font="Helvetica 20 bold").pack(pady=10)
 tk.Label(janela, text="Selecione seu peso (em kg):", font="Helvetica 15").pack(pady=5)
 
 peso = tk.StringVar(janela)
-options_peso = [int(i) for i in range(40, 200)] #
+options_peso = [int(i) for i in range(40, 200)]
 options_peso.insert(0, "Selecione")
 peso.set(options_peso[0])
 tk.OptionMenu(janela, peso, *options_peso).pack()
@@ -34,27 +50,14 @@ tk.Label(janela, text="insira sua altura em M:", font="Helvetica 15").pack(pady=
 campo_altura = Entry(janela, width=5)
 campo_altura.pack()
 
-options_peso.remove("Selecione")
-botao = Button(janela, text="Confirmar", command=calcular_imc).pack(pady=5)
+frame = tk.Frame(janela)
+frame.pack(pady=10)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+botao_confirma = Button(frame, text="Confirmar", command=calcular_imc)
+botao_confirma.grid(row=0, column=1)
+#botao_confirma.pack()
+botao_apagar = Button(frame, text="Limpar", command=limpar)
+botao_apagar.grid(row=0, column=0)
+#botao_apagar.pack()
 
 janela.mainloop()
