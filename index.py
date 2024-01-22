dca78dedf7921f3a92dd63b3ef2f0669a960bfa2
4RL3N <arlen0filho04@gmail.com>
2024-01-21 23:59:25 -0300

    Melhorada exibicao das mensagens

--- index.py
+++ index.py
@@ -3,25 +3,31 @@ from tkinter import *
 
 def infos(x):
     if x < 18.5:
-        texto = "O valor do seu IMC é considerado baixo, sendo assim, você está abaixo do peso ideal. Atenção!"
+        texto = "O valor do seu IMC é considerado baixo"
+        texto2 = "sendo assim, você está abaixo do peso ideal. Atenção!"
     elif 18.5 <= x < 24.9:
-        texto = "O valor do seu IMC está normal, ou seja, você não está nem acima do peso, nem abaixo."
+        texto = "O valor do seu IMC está normal"
+        texto2 = "ou seja, você não está nem acima do peso, nem abaixo."
     elif 25 <= x <29.9:
-        texto = "O valor do seu IMC é considerado acima do ideal, sendo assim você se enquadra em SOBREPESO. Atenção!"
+        texto = "O valor do seu IMC é considerado acima do ideal"
+        texto2 = "sendo assim você se enquadra em SOBREPESO. Atenção!"
     elif 30 <= x < 39.9:
-        texto = "O valor do seu IMC é considerado acima do ideal, sendo assim você se enquadra em OBESIDADE 1. Atenção!"
+        texto = "O valor do seu IMC é considerado acima do ideal"
+        texto2 = "sendo assim você se enquadra em OBESIDADE 1. Atenção!"
     else:
-        texto = "O valor do seu IMC é considerado acima do ideal, sendo assim você se enquadra em OBESIDADE 2. Atenção!"
-    return texto
+        texto = "O valor do seu IMC é considerado acima do ideal"
+        texto2 = "sendo assim você se enquadra em OBESIDADE 2. Atenção!"
+    return texto, texto2
 
 def janela_popup(x):
     popup = Toplevel()
-    popup.geometry("450x100")
+    popup.geometry("450x125")
     popup.title("Resultado")
     Label(popup, text=f"Seu IMC é de '{x:.2f}'", font="Helvetica 15 bold").pack()
-    texto = infos(x)
-    exibir_texto = Label(popup, text=texto, font="Helvetica 11").pack()
-    botao_fechar = Button(popup, text="Fechar", command=popup.destroy).pack(pady=5)
+    texto, texto2 = infos(x)
+    Label(popup, text=texto, font="Helvetica 12 bold").pack()
+    Label(popup, text=texto2, font="Helvetica 11").pack()
+    Button(popup, text="Fechar", command=popup.destroy).pack(pady=5)
 
 def limpar():
     peso.set(options_peso[0])
@@ -55,9 +61,7 @@ frame.pack(pady=10)
 
 botao_confirma = Button(frame, text="Confirmar", command=calcular_imc)
 botao_confirma.grid(row=0, column=1)
-#botao_confirma.pack()
 botao_apagar = Button(frame, text="Limpar", command=limpar)
 botao_apagar.grid(row=0, column=0)
-#botao_apagar.pack()
 
 janela.mainloop()
